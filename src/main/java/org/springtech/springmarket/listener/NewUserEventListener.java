7ede3eb14f5aaff5620174292249c6175493b650
Romeo <temgouaromeo@gmail.com>
2024-07-25 01:54:56 +0100

    newUserEven2

--- src/main/java/org/springtech/springmarket/listener/NewUserEventListener.java
+++ src/main/java/org/springtech/springmarket/listener/NewUserEventListener.java
@@ -14,18 +14,18 @@ import static org.springtech.springmarket.utils.RequestUtils.getDevice;
 @Component
 @RequiredArgsConstructor
 public class NewUserEventListener {
-    private final EventService eventService;
-    private final HttpServletRequest request;
-
-    @EventListener
-    public void onNewUserEvent(NewUserEvent event) {
-        try {
-            System.out.println("Received event with email: " + event.getEmail());
-            System.out.println("Device: " + getDevice(request));
-            System.out.println("IP Address: " + getIpAddress(request));
-            eventService.addUserEvent(event.getEmail(), event.getType(), getDevice(request), getIpAddress(request));
-        } catch (Exception e) {
-            e.printStackTrace(); // Ou utilisez un logger pour enregistrer l'exception
-        }
-    }
+//    private final EventService eventService;
+//    private final HttpServletRequest request;
+//
+//    @EventListener
+//    public void onNewUserEvent(NewUserEvent event) {
+//        try {
+//            System.out.println("Received event with email: " + event.getEmail());
+//            System.out.println("Device: " + getDevice(request));
+//            System.out.println("IP Address: " + getIpAddress(request));
+//            eventService.addUserEvent(event.getEmail(), event.getType(), getDevice(request), getIpAddress(request));
+//        } catch (Exception e) {
+//            e.printStackTrace(); // Ou utilisez un logger pour enregistrer l'exception
+//        }
+//    }
 }
